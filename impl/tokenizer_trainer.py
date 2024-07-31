b83b87e21aef963419db4eca1d0c8aef4af8fa11
marta1994 <marta.kosiv.lv@gmail.com>
2024-07-31 14:33:09 +0000

    refactoring

--- impl/tokenizer_trainer.py
+++ impl/tokenizer_trainer.py
@@ -32,44 +32,62 @@ class TokenizerTrainer:
                 # The original collection may be modified in the loop
                 if position not in merge_stat.positions:
                     continue
-                inp_index = position[0]
+                input_index = position[0]
                 token_index = position[1]
-                positions = self._positions[inp_index]
-                prev_token_index = positions.get_previous_index(token_index)
-                second_token_index = positions.get_next_index(token_index)
-                next_token_index = positions.get_second_next_index(token_index)
-                positions.replace_pair(token_index, next_token)
-                merge_stat.positions.remove((inp_index, token_index))
-                
-                if prev_token_index != None:
-                    pair = (positions.get_by_index(prev_token_index), merge_stat.pair[0])
-                    if pair == merge_stat.pair:
-                        merge_stat.positions.remove((inp_index, prev_token_index))
-                    else:
-                        stat = self._stats.delete_by_map_key(pair)
-                        stat.positions.remove((inp_index, prev_token_index))
-                        self._stats.push(stat)
-                    new_pair = (pair[0], next_token)
-                    stat = self._stats.delete_by_map_key(new_pair) if self._stats.contains(new_pair) else StatsEntry(new_pair, set())
-                    stat.positions.add((inp_index, prev_token_index))
-                    self._stats.push(stat)
-                        
-                if next_token_index != None:
-                    pair = (merge_stat.pair[1], positions.get_by_index(next_token_index))
-                    if pair == merge_stat.pair:
-                        merge_stat.positions.remove((inp_index, second_token_index))
-                    else:
-                        stat = self._stats.delete_by_map_key(pair)
-                        stat.positions.remove((inp_index, second_token_index))
-                        self._stats.push(stat)
-                    new_pair = (next_token, pair[1])
-                    stat = self._stats.delete_by_map_key(new_pair) if self._stats.contains(new_pair) else StatsEntry(new_pair, set())
-                    stat.positions.add((inp_index, token_index))
-                    self._stats.push(stat)
+                (left_token_index, right_token_index) = self._get_neighbor_indexes(input_index, token_index)
+                self._update_left_token(input_index, left_token_index, merge_stat, next_token)
+                self._update_right_token(input_index, right_token_index, merge_stat, next_token)
+                self._positions[input_index].replace_pair(token_index, next_token)
+                merge_stat.positions.remove((input_index, token_index))
                     
             self._merges.append(Merge(merge_stat.pair[0], merge_stat.pair[1], next_token))
             self._tokens_map[next_token] = self._tokens_map[merge_stat.pair[0]] + self._tokens_map[merge_stat.pair[1]]
             next_token += 1
+            
+    def _update_right_token(self, input_index, right_token_index, merge_stat, new_token):
+        if right_token_index == None:
+            return
+        positions = self._positions[input_index]
+        second_token_in_original_pair_index = positions.get_previous_index(right_token_index)
+        first_token_in_original_pair_index = positions.get_previous_index(second_token_in_original_pair_index)
+        pair = (merge_stat.pair[1], positions.get_by_index(right_token_index))
+        self._remove_position_from_pair(merge_stat, pair, input_index, second_token_in_original_pair_index)
+        new_pair = (new_token, pair[1])
+        self._add_position_to_pair(new_pair, input_index, first_token_in_original_pair_index)
+            
+    def _update_left_token(self, input_index, left_token_index, merge_stat, new_token):
+        if left_token_index == None:
+            return
+        positions = self._positions[input_index]
+        pair = (positions.get_by_index(left_token_index), merge_stat.pair[0])
+        self._remove_position_from_pair(merge_stat, pair, input_index, left_token_index)
+        new_pair = (pair[0], new_token)
+        self._add_position_to_pair(new_pair, input_index, left_token_index)
+        
+    def _remove_position_from_pair(self, merge_stat, pair, input_index, token_index):
+        # self._stats does not contain merge_stat, because it is currently being processed.
+        # We need to verify the current merge_stat as well as search in self._stats.
+        if pair == merge_stat.pair:
+            merge_stat.positions.remove((input_index, token_index))
+        else:
+            stat = self._stats.delete_by_map_key(pair)
+            stat.positions.remove((input_index, token_index))
+            if len(stat.positions) != 0:
+                self._stats.push(stat)
+        
+    def _add_position_to_pair(self, pair, input_index, token_index):
+        # Remove, update positions and insert again.
+        # The priority map is sorted by positions count,
+        # so to keep it sorted, we can not update positions of an object in the collection.
+        stat = self._stats.delete_by_map_key(pair) if self._stats.contains(pair) else StatsEntry(pair, set())
+        stat.positions.add((input_index, token_index))
+        self._stats.push(stat)
+            
+    def _get_neighbor_indexes(self, input_index, token_index):
+        positions = self._positions[input_index]
+        left_token_index = positions.get_previous_index(token_index)
+        right_token_index = positions.get_second_next_index(token_index)
+        return (left_token_index, right_token_index)
          
     def _calc_initial_stats(self):
         self._stats = MaxPriorityMap(
